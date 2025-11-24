7ad44bdb534594338544251a6d53a4ccb9c86b14
Pelimorita <mariana.araquer@hotmail.com>
2025-11-23 21:26:34 -0500

    DebugginSceneCurrencyChecker

--- TallerPracticoDos/Assets/Scripts/SceneCurrencyChecker.cs
+++ TallerPracticoDos/Assets/Scripts/SceneCurrencyChecker.cs
@@ -5,10 +5,10 @@ public class SceneCurrencyChecker : MonoBehaviour
     private void Start()
     {
         var managers = FindObjectsByType<CurrencyManager>(FindObjectsInactive.Include, FindObjectsSortMode.None);
-        Debug.Log($"[DEBUG] Encontradas {managers.Length} instancias de CurrencyManager en la escena.");
+       // Debug.Log($"[DEBUG] Encontradas {managers.Length} instancias de CurrencyManager en la escena.");
         foreach (var manager in managers)
         {
-            Debug.Log($"[DEBUG] CurrencyManager en objeto: {manager.gameObject.name}", manager.gameObject);
+            //Debug.Log($"[DEBUG] CurrencyManager en objeto: {manager.gameObject.name}", manager.gameObject);
         }
     }
 }
